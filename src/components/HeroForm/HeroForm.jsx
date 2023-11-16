b10d58dc38b18939825036e8124229051e205ad2
Yuri Belebekha <ybelebekha@gmail.com>
2023-11-17 00:41:27 +0200

    Saving

--- src/components/HeroForm/HeroForm.jsx
+++ src/components/HeroForm/HeroForm.jsx
@@ -1,182 +1,339 @@
-import { Formik } from 'formik';
+import { useFormik } from 'formik';
+import * as React from 'react';
 import * as yup from 'yup';
-import { Box } from '@mui/material';
+import { RadioGroup, Radio, FormControlLabel, FormControl, FormLabel } from '@mui/material';
 import {
+  SectionCss,
+  TitleCss,
   FormCss,
-  FieldCss,
-  ErrorMessageCss,
-  Title,
+  TextFieldCss,
   InputWrapperCss,
-  InputSectionCss,
-  FieldsetCss,
-  LabelCss,
-  RadioBtnBoxCss,
-  RadioBtnCss
+  InputGroupBoxCss,
 } from './HeroForm.styled';
 import { ButtonSubmit } from '../ButtonSubmit/ButtonSubmit';
 
 const heroFormValidationSchema = yup.object().shape({
   height:
     yup
-      .number()
+      .number('Enter your height')
       .integer('An integer value must be entered')
       .min(120, 'The entered value must not be less than 120 (cm)')
       .max(220, 'The entered value should not exceed 220 (cm)')
-      .required('Entering this value is required'),
+      .required('Height is required'),
   age:
     yup
-      .number()
+      .number('Enter your age')
       .integer('An integer value must be entered')
       .min(18, 'The entered value must not be less than 18 (years)')
       .max(70, 'The entered value should not exceed 70 (years)')
-      .required('Entering this value is required'),
+      .required('Age is required'),
   currentWeight:
     yup
-      .number()
+      .number('Enter your current weight')
       .integer('An integer value must be entered')
       .min(35, 'The entered value must not be less than 35 (kg)')
       .max(180, 'The entered value should not exceed 180 (kg)')
-      .required('Entering this value is required'),
+      .required('Current weight is required'),
   desiredWeight:
     yup
-      .number()
+      .number('Enter your desired weight')
       .integer('An integer value must be entered')
       .min(35, 'The entered value must not be less than 35 (kg)')
       .max(180, 'The entered value should not exceed 180 (kg)')
-      .required('Entering this value is required'),
+      .required('Desired weight is required'),
   bloodType:
     yup
-      .string()
-      .required('Entering this value is required'),
+      .string('Enter your blood type')
+      .required('Blood type is required'),
 });
 
-const initialValues = {
-  height: '',
-  age: '',
-  currentWeight: '',
-  desiredWeight: '',
-  bloodType: '1',
-};
-
 export const HeroForm = () => {
-  const handleSubmit = (values, { resetForm }) => {
-    console.log(values);
-    resetForm();
-  };
-  
+  const formik = useFormik({
+    initialValues: {
+      height: '',
+      age: '',
+      currentWeight: '',
+      desiredWeight: '',
+      bloodType: '1',
+    },
+    validationSchema: heroFormValidationSchema,
+    onSubmit: (values, { resetForm }) => {
+      console.log(values);
+      resetForm();
+    },
+  });
+
   return (
-    <Box component="section">
-      <Title component="h1">
+    <SectionCss component="section">
+      <TitleCss component="h1">
         Calculate your daily calorie intake right now
-      </Title>
+      </TitleCss>
 
-      <Formik
-        initialValues={initialValues}
-        validationSchema={heroFormValidationSchema}
-        onSubmit={handleSubmit}
-      >
-        <FormCss autoComplete='off'>
-          <InputWrapperCss>
-            <InputSectionCss>
-              <LabelCss htmlFor="height">
-                <FieldCss
-                  type="number"
-                  min="120"
-                  max="220"
-                  name="height"
-                  required
-                />
-                <span>Height *</span>
-                <ErrorMessageCss component="div" name="height" />
-              </LabelCss>
-              
-              <LabelCss htmlFor="age">
-                <FieldCss
-                  type="number"
-                  min="18"
-                  max="70"
-                  name="age"
-                  required
-                />
-                <span>Age *</span>
-                <ErrorMessageCss component="div" name="age" />
-              </LabelCss>
+      <FormCss onSubmit={formik.handleSubmit}>
+        <InputWrapperCss>
+          <InputGroupBoxCss>
+            <TextFieldCss
+              variant="standard"
+              id="height"
+              name="height"
+              label="Height *"
+              type="number"
+              value={formik.values.height}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.height && Boolean(formik.errors.height)}
+              helperText={formik.touched.height && formik.errors.height}
+            />
+            
+            <TextFieldCss
+              variant="standard"
+              id="age"
+              name="age"
+              label="Age *"
+              type="number"
+              value={formik.values.age}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.age && Boolean(formik.errors.age)}
+              helperText={formik.touched.age && formik.errors.age}
+            />
 
-              <LabelCss htmlFor="currentWeight">
-                <FieldCss
-                  type="number"
-                  min="35"
-                  max="180"
-                  name="currentWeight"
-                  required
-                />
-                <span>Current weight *</span>
-                <ErrorMessageCss component="div" name="currentWeight" />
-              </LabelCss>
-            </InputSectionCss>
+            <TextFieldCss
+              variant="standard"
+              id="currentWeight"
+              name="currentWeight"
+              label="Current weight *"
+              type="number"
+              value={formik.values.currentWeight}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.currentWeight && Boolean(formik.errors.currentWeight)}
+              helperText={formik.touched.currentWeight && formik.errors.currentWeight}
+            />
+          </InputGroupBoxCss>
 
-            <InputSectionCss>
-              <LabelCss htmlFor="desiredWeight">
-                <FieldCss
-                  type="number"
-                  min="35"
-                  max="180"
-                  name="desiredWeight"
-                  required
-                />
-                <span>Desired weight *</span>
-                <ErrorMessageCss component="div" name="desiredWeight" />
-              </LabelCss>            
+          <InputGroupBoxCss>
+            <TextFieldCss
+              variant="standard"
+              id="desiredWeight"
+              name="desiredWeight"
+              label="Desired weight *"
+              type="number"
+              value={formik.values.desiredWeight}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.desiredWeight && Boolean(formik.errors.desiredWeight)}
+              helperText={formik.touched.desiredWeight && formik.errors.desiredWeight}
+            />
 
-              <FieldsetCss>
-                <legend>Blood type *</legend>
+            <FormControl>
+              <FormLabel id="bloodType">Blood type *</FormLabel>
 
-                <RadioBtnBoxCss>
-                  <label htmlFor="1">
-                    <RadioBtnCss
-                      type="radio"
-                      value="1"
-                      name="bloodType"
-                    />
-                    <span>1</span>
-                  </label>
+              <RadioGroup
+                row
+                aria-labelledby="bloodType"
+                name="bloodType"
+                value={formik.values.bloodType}
+                onChange={formik.handleChange}
+              >
+                <FormControlLabel value="1" control={<Radio />} label="1" />
+                <FormControlLabel value="2" control={<Radio />} label="2" />
+                <FormControlLabel value="3" control={<Radio />} label="3" />
+                <FormControlLabel value="4" control={<Radio />} label="4" />
+              </RadioGroup>
+            </FormControl>
+          </InputGroupBoxCss>
+        </InputWrapperCss>
 
-                  <label htmlFor="2">
-                    <RadioBtnCss
-                      type="radio"
-                      value="2"
-                      name="bloodType"
-                    />
-                    <span>2</span>
-                  </label>
-
-                  <label htmlFor="3">
-                    <RadioBtnCss
-                      type="radio"
-                      value="3"
-                      name="bloodType"
-                    />
-                    <span>3</span>
-                  </label>
-                  
-                  <label htmlFor="4">
-                    <RadioBtnCss
-                      type="radio"
-                      value="4"
-                      name="bloodType"
-                    />
-                    <span>4</span>
-                  </label>
-                </RadioBtnBoxCss>
-                
-                <ErrorMessageCss component="div" name="bloodType" />  
-              </FieldsetCss>
-            </InputSectionCss>
-          </InputWrapperCss>
-          
-          <ButtonSubmit name="Start losing weight" />
-        </FormCss>
-      </Formik>
-    </Box>
-  );
+        <ButtonSubmit name="Start losing weight" />
+      </FormCss>
+    </SectionCss>
+  )
 };
+
+
+
+
+
+// import { Formik } from 'formik';
+// import * as yup from 'yup';
+// import { Box } from '@mui/material';
+// import {
+//   FormCss,
+//   FieldCss,
+//   ErrorMessageCss,
+//   Title,
+//   InputWrapperCss,
+//   InputSectionCss,
+//   FieldsetCss,
+//   LabelCss,
+//   RadioBtnBoxCss,
+//   RadioBtnCss
+// } from './HeroForm.styled';
+// import { ButtonSubmit } from '../ButtonSubmit/ButtonSubmit';
+
+// const heroFormValidationSchema = yup.object().shape({
+//   height:
+//     yup
+//       .number()
+//       .integer('An integer value must be entered')
+//       .min(120, 'The entered value must not be less than 120 (cm)')
+//       .max(220, 'The entered value should not exceed 220 (cm)')
+//       .required('Entering this value is required'),
+//   age:
+//     yup
+//       .number()
+//       .integer('An integer value must be entered')
+//       .min(18, 'The entered value must not be less than 18 (years)')
+//       .max(70, 'The entered value should not exceed 70 (years)')
+//       .required('Entering this value is required'),
+//   currentWeight:
+//     yup
+//       .number()
+//       .integer('An integer value must be entered')
+//       .min(35, 'The entered value must not be less than 35 (kg)')
+//       .max(180, 'The entered value should not exceed 180 (kg)')
+//       .required('Entering this value is required'),
+//   desiredWeight:
+//     yup
+//       .number()
+//       .integer('An integer value must be entered')
+//       .min(35, 'The entered value must not be less than 35 (kg)')
+//       .max(180, 'The entered value should not exceed 180 (kg)')
+//       .required('Entering this value is required'),
+//   bloodType:
+//     yup
+//       .string()
+//       .required('Entering this value is required'),
+// });
+
+// const initialValues = {
+//   height: '',
+//   age: '',
+//   currentWeight: '',
+//   desiredWeight: '',
+//   bloodType: '1',
+// };
+
+// export const HeroForm = () => {
+//   const handleSubmit = (values, { resetForm }) => {
+//     console.log(values);
+//     resetForm();
+//   };
+  
+//   return (
+//     <Box component="section">
+//       <Title component="h1">
+//         Calculate your daily calorie intake right now
+//       </Title>
+
+//       <Formik
+//         initialValues={initialValues}
+//         validationSchema={heroFormValidationSchema}
+//         onSubmit={handleSubmit}
+//       >
+//         <FormCss autoComplete='off'>
+//           <InputWrapperCss>
+//             <InputSectionCss>
+//               <LabelCss htmlFor="height">
+//                 <FieldCss
+//                   type="number"
+//                   min="120"
+//                   max="220"
+//                   name="height"
+//                   required
+//                 />
+//                 <span>Height *</span>
+//                 <ErrorMessageCss component="div" name="height" />
+//               </LabelCss>
+              
+//               <LabelCss htmlFor="age">
+//                 <FieldCss
+//                   type="number"
+//                   min="18"
+//                   max="70"
+//                   name="age"
+//                   required
+//                 />
+//                 <span>Age *</span>
+//                 <ErrorMessageCss component="div" name="age" />
+//               </LabelCss>
+
+//               <LabelCss htmlFor="currentWeight">
+//                 <FieldCss
+//                   type="number"
+//                   min="35"
+//                   max="180"
+//                   name="currentWeight"
+//                   required
+//                 />
+//                 <span>Current weight *</span>
+//                 <ErrorMessageCss component="div" name="currentWeight" />
+//               </LabelCss>
+//             </InputSectionCss>
+
+//             <InputSectionCss>
+//               <LabelCss htmlFor="desiredWeight">
+//                 <FieldCss
+//                   type="number"
+//                   min="35"
+//                   max="180"
+//                   name="desiredWeight"
+//                   required
+//                 />
+//                 <span>Desired weight *</span>
+//                 <ErrorMessageCss component="div" name="desiredWeight" />
+//               </LabelCss>            
+
+//               <FieldsetCss>
+//                 <legend>Blood type *</legend>
+
+//                 <RadioBtnBoxCss>
+//                   <label htmlFor="1">
+//                     <RadioBtnCss
+//                       type="radio"
+//                       value="1"
+//                       name="bloodType"
+//                     />
+//                     <span>1</span>
+//                   </label>
+
+//                   <label htmlFor="2">
+//                     <RadioBtnCss
+//                       type="radio"
+//                       value="2"
+//                       name="bloodType"
+//                     />
+//                     <span>2</span>
+//                   </label>
+
+//                   <label htmlFor="3">
+//                     <RadioBtnCss
+//                       type="radio"
+//                       value="3"
+//                       name="bloodType"
+//                     />
+//                     <span>3</span>
+//                   </label>
+                  
+//                   <label htmlFor="4">
+//                     <RadioBtnCss
+//                       type="radio"
+//                       value="4"
+//                       name="bloodType"
+//                     />
+//                     <span>4</span>
+//                   </label>
+//                 </RadioBtnBoxCss>
+                
+//                 <ErrorMessageCss component="div" name="bloodType" />  
+//               </FieldsetCss>
+//             </InputSectionCss>
+//           </InputWrapperCss>
+          
+//           <ButtonSubmit name="Start losing weight" />
+//         </FormCss>
+//       </Formik>
+//     </Box>
+//   );
+// };
--- src/components/HeroForm/HeroForm.styled.jsx
+++ src/components/HeroForm/HeroForm.styled.jsx
@@ -1,167 +1,271 @@
-import { Form, Field, ErrorMessage } from 'formik';
-import { Typography } from '@mui/material';
 import styled from '@emotion/styled';
+import { Box, Typography, TextField } from '@mui/material';
 import { theme } from '../../constants/theme';
 
-export const Title = styled(Typography)`
-  max-width: 280px;
-  margin-bottom: 34px;
+export const SectionCss = styled(Box)`
+  max-width: 240px;
+
+  ${theme.breakpoints.up('tablet')} {
+    max-width: 530px;
+  };
+`;
+
+export const TitleCss = styled(Typography)`
+  text-align: center;
+  margin-bottom: 55px;
 
   font-family: VerdanaBold;
   font-weight: 700;
   font-size: 18px;
   line-height: 25px;
+  color: ${theme.palette.primary.main};
+
+  
 
   ${theme.breakpoints.up('tablet')} {
-    max-width: 530px;
     margin-bottom: 68px;
-
+    
     font-size: 34px;
     line-height: 48px;
+    text-align: left;
   };
 `;
 
-export const FormCss = styled(Form)`  
+export const FormCss = styled.form`
   display: flex;
   flex-direction: column;
-  align-items: center;
-  max-width: 240px;
+  align-items: center;  
 
   ${theme.breakpoints.up('tablet')} {
-    max-width: 530px;
     align-items: start;
   };
-
-  ${theme.breakpoints.up('desktop')} {
-    max-width: 530px;
-    align-items: end;
-  };
 `;
 
-export const InputWrapperCss = styled.div`
-  display: flex;
-  column-gap: 32px;
-  flex-wrap: wrap;
-  margin-bottom: 8px;
+export const TextFieldCss = styled(TextField)`
+  width: 280px;
+  padding: 4px 0;
+  margin-bottom: 32px;  
+  
+  & label {
+    font-family: VerdanaBold;
+    font-size: 14px;
+    font-weight: 700;
+    line-height: 17px;
+    letter-spacing: 0.04em;
+  };
+
+  & label.Mui-focused {
+    color: ${theme.palette.primary.accent};
+  };
+
+  & .MuiInput-underline:after {
+    border-bottom-color: ${theme.palette.primary.accent};    
+  };
+
+  & .MuiInput-root {
+    font-family: VerdanaBold;
+    font-size: 14px;
+    font-weight: 700;
+    line-height: 17px;
+    letter-spacing: 0.04em;
+    background: transparent;
+  };
 
   ${theme.breakpoints.up('tablet')} {
-    margin-bottom: 28px;
+    width: 240px;
   };
 `;
 
-export const InputSectionCss = styled.div`
+export const InputWrapperCss = styled(Box)`
+  display: flex;
+`;
+
+export const InputGroupBoxCss = styled(Box)`
   display: flex;
   flex-direction: column;
 `;
 
-export const FieldCss = styled(Field)`
-  width: 240px;
-  padding: 4px 0;
-  margin-bottom: 32px;
 
-  background-color: transparent;
-  
-  border-top: none;
-  border-left: none;
-  border-bottom: 1px solid;
-  border-right: none;
-  border-color: ${theme.palette.primary.grey};
-  outline: none;  
 
-  font-size: 14px;
-  font-weight: 700;
-  line-height: 17px;
-  letter-spacing: 0.04em;
 
-  transition: border-color ${theme.transitions.duration.standard}ms ${theme.transitions.easing.easeOut};
-
-  &:hover,
-  &:focus {
-    border-color: ${theme.palette.primary.accent};
-  };
-  
-  & ~ span {
-    position: absolute;
-    top: 0;
-    left: 0;
-  };
-
-  &:focus ~ span,
-  &:not(:focus):valid ~ span {
-    top: -16px;
-    font-size: 10px;
-    line-height: 14px;
-    color: ${theme.palette.primary.accent};
-  };
-`;
-
-export const ErrorMessageCss = styled(ErrorMessage)`
-  position: absolute;
-  top: 30px;
-  left: 0;
-  max-width: 240px;
-  font-size: 7px;
-  line-height: 10px;
-  color: ${theme.palette.primary.errorMessage};
-`;
-
-export const FieldsetCss = styled.fieldset`
-  border: none;
-  margin: 0;
-  padding: 0;
-  
-  font-size: 14px;
-  font-weight: 700;
-  line-height: 17px;
-  letter-spacing: 0.04em;
-  color: ${theme.palette.primary.darkGrey};  
-`;
-
-export const LabelCss = styled.label`
-  position: relative;
-
-  font-size: 14px;
-  font-weight: 700;
-  line-height: 17px;
-  letter-spacing: 0.04em;
-  color: ${theme.palette.primary.darkGrey};
-`;
-
-export const RadioBtnBoxCss = styled.div`
+export const ButtonsBoxCss = styled(Box)`
   display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-top: 8px;
-  margin-bottom: 40px;
-
-  & > label {
-    display: flex;
-    align-items: center;    
-  };
-
-  & > label > span {
-    margin-left: 4px;
-  };
+  flex-direction: column;
+  margin-top: 28px;
 
   ${theme.breakpoints.up('tablet')} {
-    padding-top: 8px;
-    border-top: 1px solid ${theme.palette.primary.grey};
+    flex-direction: row;
   };
 `;
 
-export const RadioBtnCss = styled(Field)`
-  width: 20px;
-  height: 20px;
+    
 
-  &:checked {
-    appearance: none;
-    border: 5px solid ${theme.palette.primary.white};
-    border-radius: 50%;
-    box-shadow: ${theme.palette.primary.darkGrey} 0 0 0 1px;
-    background: ${theme.palette.primary.accent};
 
-    & ~ span {
-      color: ${theme.palette.primary.accent};
-    };
-  };
-`;
+
+// import { Form, Field, ErrorMessage } from 'formik';
+// import { Typography } from '@mui/material';
+// import styled from '@emotion/styled';
+// import { theme } from '../../constants/theme';
+
+// export const Title = styled(Typography)`
+//   max-width: 280px;
+//   margin-bottom: 34px;
+
+//   font-family: VerdanaBold;
+//   font-weight: 700;
+//   font-size: 18px;
+//   line-height: 25px;
+
+//   ${theme.breakpoints.up('tablet')} {
+//     max-width: 530px;
+//     margin-bottom: 68px;
+
+//     font-size: 34px;
+//     line-height: 48px;
+//   };
+// `;
+
+// export const FormCss = styled(Form)`  
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+  // max-width: 240px;
+
+  // ${theme.breakpoints.up('tablet')} {
+  //   max-width: 530px;
+  //   align-items: start;
+  // };
+
+  // ${theme.breakpoints.up('desktop')} {
+  //   max-width: 530px;
+  //   align-items: end;
+  // };
+// `;
+
+// export const InputWrapperCss = styled.div`
+//   display: flex;
+//   column-gap: 32px;
+//   flex-wrap: wrap;
+//   margin-bottom: 8px;
+
+//   ${theme.breakpoints.up('tablet')} {
+//     margin-bottom: 28px;
+//   };
+// `;
+
+// export const InputSectionCss = styled.div`
+//   display: flex;
+//   flex-direction: column;
+// `;
+
+// export const FieldCss = styled(Field)`
+//   width: 240px;
+//   padding: 4px 0;
+//   margin-bottom: 32px;
+
+//   background-color: transparent;
+  
+//   border-top: none;
+//   border-left: none;
+//   border-bottom: 1px solid;
+//   border-right: none;
+//   border-color: ${theme.palette.primary.grey};
+//   outline: none;  
+
+//   font-size: 14px;
+//   font-weight: 700;
+//   line-height: 17px;
+//   letter-spacing: 0.04em;
+
+//   transition: border-color ${theme.transitions.duration.standard}ms ${theme.transitions.easing.easeOut};
+
+//   &:hover,
+//   &:focus {
+//     border-color: ${theme.palette.primary.accent};
+//   };
+  
+//   & ~ span {
+//     position: absolute;
+//     top: 0;
+//     left: 0;
+//   };
+
+//   &:focus ~ span,
+//   &:not(:focus):valid ~ span {
+//     top: -16px;
+//     font-size: 10px;
+//     line-height: 14px;
+//     color: ${theme.palette.primary.accent};
+//   };
+// `;
+
+// export const ErrorMessageCss = styled(ErrorMessage)`
+//   position: absolute;
+//   top: 30px;
+//   left: 0;
+//   max-width: 240px;
+//   font-size: 7px;
+//   line-height: 10px;
+//   color: ${theme.palette.primary.errorMessage};
+// `;
+
+// export const FieldsetCss = styled.fieldset`
+//   border: none;
+//   margin: 0;
+//   padding: 0;
+  
+//   font-size: 14px;
+//   font-weight: 700;
+//   line-height: 17px;
+//   letter-spacing: 0.04em;
+//   color: ${theme.palette.primary.darkGrey};  
+// `;
+
+// export const LabelCss = styled.label`
+//   position: relative;
+
+//   font-size: 14px;
+//   font-weight: 700;
+//   line-height: 17px;
+//   letter-spacing: 0.04em;
+//   color: ${theme.palette.primary.darkGrey};
+// `;
+
+// export const RadioBtnBoxCss = styled.div`
+//   display: flex;
+//   justify-content: space-between;
+//   align-items: center;
+//   margin-top: 8px;
+//   margin-bottom: 40px;
+
+//   & > label {
+//     display: flex;
+//     align-items: center;    
+//   };
+
+//   & > label > span {
+//     margin-left: 4px;
+//   };
+
+//   ${theme.breakpoints.up('tablet')} {
+//     padding-top: 8px;
+//     border-top: 1px solid ${theme.palette.primary.grey};
+//   };
+// `;
+
+// export const RadioBtnCss = styled(Field)`
+//   width: 20px;
+//   height: 20px;
+
+//   &:checked {
+//     appearance: none;
+//     border: 5px solid ${theme.palette.primary.white};
+//     border-radius: 50%;
+//     box-shadow: ${theme.palette.primary.darkGrey} 0 0 0 1px;
+//     background: ${theme.palette.primary.accent};
+
+//     & ~ span {
+//       color: ${theme.palette.primary.accent};
+//     };
+//   };
+// `;
