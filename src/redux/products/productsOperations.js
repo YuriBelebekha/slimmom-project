b8e92c579398b33aed116c7c88e89905b02b64e5
Yuri Belebekha <ybelebekha@gmail.com>
2024-01-03 01:10:34 +0200

    Create new dir products/redux

---
+++ src/redux/products/productsOperations.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { toast } from 'react-toastify';
+import { ToastOptions } from 'services/toast-options';
+
+axios.defaults.baseURL = `${process.env.REACT_APP_BASE_API_URL}`;
+
+const token = {
+  set(token) {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  },
+  unset() {
+    axios.defaults.headers.common.Authorization = '';
+  },
+};
+
+// GET - '/product' - Select and get a list of products by query
