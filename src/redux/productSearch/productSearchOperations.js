7b4dc79fc0824edb44f2516a891e9708a08c95d1
Yuri Belebekha <ybelebekha@gmail.com>
2024-01-11 21:17:34 +0200

    Renamed productOperations to productSearchOperations

--- src/redux/productSearch/productSearchOperations.js
+++ src/redux/productSearch/productSearchOperations.js
@@ -1,18 +1,18 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { toast } from 'react-toastify';
-import { ToastOptions } from 'services/toast-options';
+// import { toast } from 'react-toastify';
+// import { ToastOptions } from 'services/toast-options';
 
 axios.defaults.baseURL = `${process.env.REACT_APP_BASE_API_URL}`;
 
-const token = {
-  set(token) {
-    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-  },
-  unset() {
-    axios.defaults.headers.common.Authorization = '';
-  },
-};
+// const token = {
+//   set(token) {
+//     axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+//   },
+//   unset() {
+//     axios.defaults.headers.common.Authorization = '';
+//   },
+// };
 
 // GET - '/product' - Select and get a list of products by query
 export const selectProduct = createAsyncThunk(
@@ -30,5 +30,3 @@ export const selectProduct = createAsyncThunk(
     // }
   }
 );
-
-selectProduct();
