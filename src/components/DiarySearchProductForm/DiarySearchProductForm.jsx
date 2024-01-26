4e9988c9d7ff4c9b0730c86c26581ec2082f364a
Yuri Belebekha <ybelebekha@gmail.com>
2024-01-27 00:08:07 +0200

    Saving

--- src/components/DiarySearchProductForm/DiarySearchProductForm.jsx
+++ src/components/DiarySearchProductForm/DiarySearchProductForm.jsx
@@ -1,14 +1,16 @@
+import { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { store } from '../../redux/store';
+import { debounce } from 'lodash';
 import { getProductsFromList } from 'redux/productSearch/productSearchOperations';
 import { useFormik } from 'formik';
 import * as yup from 'yup';
 
+import { useScreenSize } from '../../hooks/useScreenSize';
+import { store } from '../../redux/store';
+import { theme } from '../../constants/theme';
 import { VisuallyHidden } from '../VisuallyHidden';
 import { ButtonSubmit } from '../ButtonSubmit/ButtonSubmit';
-import { useScreenSize } from '../../hooks/useScreenSize';
 
-import { theme } from '../../constants/theme';
 import { Box, Autocomplete } from '@mui/material';
 import { FormCss, TextFieldCss } from './DiarySearchProductForm.styled';
 
@@ -27,20 +29,25 @@ const searchProductFormValidationSchema = yup.object().shape({
 });
 
 export const DiarySearchProductForm = () => {
-  const dispatch = useDispatch();
   const screenSize = useScreenSize();
 
   const {
     products: { foundProductsList },
   } = store.getState();
+  // console.log(foundProductsList);
 
-  const handleChange = e => {
-    dispatch(getProductsFromList(e.target.value));
+  const dispatch = useDispatch();
+  dispatch(getProductsFromList());
+
+  const [productName, setProductName] = useState('');
+
+  const handleInputChange = e => {
+    const inputValue = e.target.value;
+    setProductName(inputValue);
   };
+  const debouncedOnInputChange = debounce(handleInputChange, 1500);
 
-  function capitalizeFirstLetter(string) {
-    return string[0].toUpperCase() + string.slice(1);
-  }
+  console.log(productName);
 
   const formik = useFormik({
     initialValues: {
@@ -51,12 +58,11 @@ export const DiarySearchProductForm = () => {
     validationSchema: searchProductFormValidationSchema,
 
     onSubmit: (values, { resetForm, setSubmitting }) => {
-      console.log(values);
-
       const payload = {
         search: values.search,
         weight: values.weight,
       };
+
       dispatch(getProductsFromList(payload)).finally(() => {
         setSubmitting(false);
       });
@@ -65,28 +71,155 @@ export const DiarySearchProductForm = () => {
     },
   });
 
+  const top100Films = [
+    { title: 'The Shawshank Redemption', year: 1994 },
+    { title: 'The Godfather', year: 1972 },
+    { title: 'The Godfather: Part II', year: 1974 },
+    { title: 'The Dark Knight', year: 2008 },
+    { title: '12 Angry Men', year: 1957 },
+    { title: "Schindler's List", year: 1993 },
+    { title: 'Pulp Fiction', year: 1994 },
+    {
+      title: 'The Lord of the Rings: The Return of the King',
+      year: 2003,
+    },
+    { title: 'The Good, the Bad and the Ugly', year: 1966 },
+    { title: 'Fight Club', year: 1999 },
+    {
+      title: 'The Lord of the Rings: The Fellowship of the Ring',
+      year: 2001,
+    },
+    {
+      title: 'Star Wars: Episode V - The Empire Strikes Back',
+      year: 1980,
+    },
+    { title: 'Forrest Gump', year: 1994 },
+    { title: 'Inception', year: 2010 },
+    {
+      title: 'The Lord of the Rings: The Two Towers',
+      year: 2002,
+    },
+    { title: "One Flew Over the Cuckoo's Nest", year: 1975 },
+    { title: 'Goodfellas', year: 1990 },
+    { title: 'The Matrix', year: 1999 },
+    { title: 'Seven Samurai', year: 1954 },
+    {
+      title: 'Star Wars: Episode IV - A New Hope',
+      year: 1977,
+    },
+    { title: 'City of God', year: 2002 },
+    { title: 'Se7en', year: 1995 },
+    { title: 'The Silence of the Lambs', year: 1991 },
+    { title: "It's a Wonderful Life", year: 1946 },
+    { title: 'Life Is Beautiful', year: 1997 },
+    { title: 'The Usual Suspects', year: 1995 },
+    { title: 'Léon: The Professional', year: 1994 },
+    { title: 'Spirited Away', year: 2001 },
+    { title: 'Saving Private Ryan', year: 1998 },
+    { title: 'Once Upon a Time in the West', year: 1968 },
+    { title: 'American History X', year: 1998 },
+    { title: 'Interstellar', year: 2014 },
+    { title: 'Casablanca', year: 1942 },
+    { title: 'City Lights', year: 1931 },
+    { title: 'Psycho', year: 1960 },
+    { title: 'The Green Mile', year: 1999 },
+    { title: 'The Intouchables', year: 2011 },
+    { title: 'Modern Times', year: 1936 },
+    { title: 'Raiders of the Lost Ark', year: 1981 },
+    { title: 'Rear Window', year: 1954 },
+    { title: 'The Pianist', year: 2002 },
+    { title: 'The Departed', year: 2006 },
+    { title: 'Terminator 2: Judgment Day', year: 1991 },
+    { title: 'Back to the Future', year: 1985 },
+    { title: 'Whiplash', year: 2014 },
+    { title: 'Gladiator', year: 2000 },
+    { title: 'Memento', year: 2000 },
+    { title: 'The Prestige', year: 2006 },
+    { title: 'The Lion King', year: 1994 },
+    { title: 'Apocalypse Now', year: 1979 },
+    { title: 'Alien', year: 1979 },
+    { title: 'Sunset Boulevard', year: 1950 },
+    {
+      title:
+        'Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb',
+      year: 1964,
+    },
+    { title: 'The Great Dictator', year: 1940 },
+    { title: 'Cinema Paradiso', year: 1988 },
+    { title: 'The Lives of Others', year: 2006 },
+    { title: 'Grave of the Fireflies', year: 1988 },
+    { title: 'Paths of Glory', year: 1957 },
+    { title: 'Django Unchained', year: 2012 },
+    { title: 'The Shining', year: 1980 },
+    { title: 'WALL·E', year: 2008 },
+    { title: 'American Beauty', year: 1999 },
+    { title: 'The Dark Knight Rises', year: 2012 },
+    { title: 'Princess Mononoke', year: 1997 },
+    { title: 'Aliens', year: 1986 },
+    { title: 'Oldboy', year: 2003 },
+    { title: 'Once Upon a Time in America', year: 1984 },
+    { title: 'Witness for the Prosecution', year: 1957 },
+    { title: 'Das Boot', year: 1981 },
+    { title: 'Citizen Kane', year: 1941 },
+    { title: 'North by Northwest', year: 1959 },
+    { title: 'Vertigo', year: 1958 },
+    {
+      title: 'Star Wars: Episode VI - Return of the Jedi',
+      year: 1983,
+    },
+    { title: 'Reservoir Dogs', year: 1992 },
+    { title: 'Braveheart', year: 1995 },
+    { title: 'M', year: 1931 },
+    { title: 'Requiem for a Dream', year: 2000 },
+    { title: 'Amélie', year: 2001 },
+    { title: 'A Clockwork Orange', year: 1971 },
+    { title: 'Like Stars on Earth', year: 2007 },
+    { title: 'Taxi Driver', year: 1976 },
+    { title: 'Lawrence of Arabia', year: 1962 },
+    { title: 'Double Indemnity', year: 1944 },
+    {
+      title: 'Eternal Sunshine of the Spotless Mind',
+      year: 2004,
+    },
+    { title: 'Amadeus', year: 1984 },
+    { title: 'To Kill a Mockingbird', year: 1962 },
+    { title: 'Toy Story 3', year: 2010 },
+    { title: 'Logan', year: 2017 },
+    { title: 'Full Metal Jacket', year: 1987 },
+    { title: 'Dangal', year: 2016 },
+    { title: 'The Sting', year: 1973 },
+    { title: '2001: A Space Odyssey', year: 1968 },
+    { title: "Singin' in the Rain", year: 1952 },
+    { title: 'Toy Story', year: 1995 },
+    { title: 'Bicycle Thieves', year: 1948 },
+    { title: 'The Kid', year: 1921 },
+    { title: 'Inglourious Basterds', year: 2009 },
+    { title: 'Snatch', year: 2000 },
+    { title: '3 Idiots', year: 2009 },
+    { title: 'Monty Python and the Holy Grail', year: 1975 },
+  ];
+
   return (
     <Box>
       <VisuallyHidden component="h2">Search Product Form</VisuallyHidden>
 
       <FormCss onSubmit={formik.handleSubmit}>
         <Autocomplete
-          disablePortal
+          freeSolo
           id="search"
-          onInputChange={handleChange}
-          options={foundProductsList.map(option =>
-            capitalizeFirstLetter(option.title.en)
-          )}
-          sx={{ width: 300 }}
-          renderOption={(props, option) => {
-            return (
-              <li {...props} key={props.id}>
-                {option}
-              </li>
-            );
-          }}
+          disableClearable
+          inputValue={productName}
+          onInputChange={debouncedOnInputChange}
+          options={foundProductsList.map(option => option.title.en)}
           renderInput={params => (
-            <TextFieldCss {...params} label="Enter product name" />
+            <TextFieldCss
+              {...params}
+              label="Enter product name"
+              InputProps={{
+                ...params.InputProps,
+                type: 'search',
+              }}
+            />
           )}
         />
 
@@ -111,6 +244,110 @@ export const DiarySearchProductForm = () => {
     </Box>
   );
 };
+
+// export const DiarySearchProductForm = () => {
+//   const dispatch = useDispatch();
+//   const screenSize = useScreenSize();
+//   const [productName, setProductName] = useState('');
+//   const [inputValue, setInputValue] = useState('');
+//   const [productId, setProductId] = useState('');
+
+//   const {
+//     products: { foundProductsList },
+//   } = store.getState();
+//   // console.log(foundProductsList);
+
+//   const handleInputChange = (event, value) => {
+//     dispatch(getProductsFromList(event.target.value));
+//     setInputValue(value);
+//   };
+
+//   const handleChange = (_event, value) => {
+//     setProductName(value);
+//     console.log(value);
+
+//     foundProductsList.map(option => {
+//       return option.title.en.toLowerCase() === value.toLowerCase()
+//         ? setProductId(option._id)
+//         : setProductId('');
+//     });
+//   };
+
+//   function capitalizeFirstLetter(string) {
+//     return string[0].toUpperCase() + string.slice(1);
+//   }
+
+//   const formik = useFormik({
+//     initialValues: {
+//       search: '',
+//       weight: '',
+//     },
+
+//     validationSchema: searchProductFormValidationSchema,
+
+//     onSubmit: (values, { resetForm, setSubmitting }) => {
+//       const payload = {
+//         search: productId,
+//         weight: values.weight,
+//       };
+
+//       dispatch(getProductsFromList(payload)).finally(() => {
+//         setSubmitting(false);
+//       });
+
+//       resetForm();
+//     },
+//   });
+
+//   return (
+//     <Box>
+//       <VisuallyHidden component="h2">Search Product Form</VisuallyHidden>
+
+//       <FormCss onSubmit={formik.handleSubmit}>
+//         <Autocomplete
+//           disablePortal
+//           id="search"
+//           value={productName}
+//           inputValue={inputValue}
+//           onInputChange={handleInputChange}
+//           onChange={handleChange}
+//           options={foundProductsList.map(option =>
+//             capitalizeFirstLetter(option.title.en)
+//           )}
+//           sx={{ width: 300 }}
+//           renderOption={(props, option) => {
+//             return (
+//               <li {...props} key={props.id}>
+//                 {option}
+//               </li>
+//             );
+//           }}
+//           renderInput={params => (
+//             <TextFieldCss {...params} label="Enter product name" />
+//           )}
+//         />
+
+//         <TextFieldCss
+//           variant="standard"
+//           id="weight"
+//           name="weight"
+//           label="Enter grams"
+//           value={formik.values.weight}
+//           onChange={formik.handleChange}
+//           onBlur={formik.handleBlur}
+//           error={formik.touched.weight && Boolean(formik.errors.weight)}
+//           helperText={formik.touched.weight && formik.errors.weight}
+//         />
+
+//         {screenSize.width < theme.breakpoints.values.tablet ? (
+//           <ButtonSubmit name="Add" />
+//         ) : (
+//           <ButtonSubmit name="+" />
+//         )}
+//       </FormCss>
+//     </Box>
+//   );
+// };
 
 // import { useDispatch } from 'react-redux';
 // import { selectProductFromList } from 'redux/productSearch/productSearchOperations';
