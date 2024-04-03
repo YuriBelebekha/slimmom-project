821dc09e3fd410566ff274eb4bb9aa6e26cdcabc
Yuri Belebekha <ybelebekha@gmail.com>
2024-04-03 23:15:29 +0300

    Saving

--- src/components/DiaryUserCalendar/DiaryUserCalendar.jsx
+++ src/components/DiaryUserCalendar/DiaryUserCalendar.jsx
@@ -37,6 +37,7 @@ export const DiaryUserCalendar = () => {
       selectedDate = value;
       dispatch(getInfoForDay({ date: convertDate(selectedDate) })).then(
         args => {
+          // console.log('args: ', args);
           setDayInfo(prevState => {
             if (!isEqual(prevState, args.payload)) {
               return { ...args.payload };
@@ -49,9 +50,13 @@ export const DiaryUserCalendar = () => {
     [dispatch]
   );
 
+  // console.log('dayInfo: ', dayInfo);
+
   useEffect(() => {
     handleAcceptDate(selectedDate);
   }, [dayInfo, handleAcceptDate]);
+
+  // console.log('dayInfo after useEffect: ', dayInfo);
 
   return (
     <>
@@ -79,3 +84,86 @@ export const DiaryUserCalendar = () => {
     </>
   );
 };
+
+// import { useState, useEffect, useCallback } from 'react';
+// import { useDispatch } from 'react-redux';
+// import dayjs from 'dayjs';
+// import { isEqual } from 'lodash';
+
+// import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+// import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+// import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
+// import DateRangeIcon from '@mui/icons-material/DateRange';
+
+// import { DiarySearchProductMobile } from '../DiarySearchProductMobile';
+// import { DiarySearchProduct } from '../DiarySearchProduct/DiarySearchProduct';
+// import { DiaryEatenProductsList } from '../DairyEatenProductsList/DairyEatenProductsList';
+// import { useScreenSize } from '../../hooks/useScreenSize';
+// import { getInfoForDay } from 'redux/day/dayOperations';
+// import { theme } from '../../constants/theme';
+
+// import {
+//   DatePickerBoxCss,
+//   DiaryListProductsBoxCss,
+// } from './DiaryUserCalendar.styled';
+
+// const currentDate = new Date();
+// export let selectedDate = currentDate;
+
+// function convertDate(date) {
+//   return String(dayjs(date).format('YYYY-MM-DD'));
+// }
+
+// export const DiaryUserCalendar = () => {
+//   const [dayInfo, setDayInfo] = useState('');
+//   const screenSize = useScreenSize();
+//   const dispatch = useDispatch();
+
+//   const handleAcceptDate = useCallback(
+//     value => {
+//       selectedDate = value;
+//       dispatch(getInfoForDay({ date: convertDate(selectedDate) })).then(
+//         args => {
+//           console.log('args: ', args);
+//           setDayInfo(prevState => {
+//             if (!isEqual(prevState, args.payload)) {
+//               return { ...args.payload };
+//             }
+//             return prevState;
+//           });
+//         }
+//       );
+//     },
+//     [dispatch]
+//   );
+
+//   useEffect(() => {
+//     handleAcceptDate(selectedDate);
+//   }, [dayInfo, handleAcceptDate]);
+
+//   return (
+//     <>
+//       <DatePickerBoxCss>
+//         <LocalizationProvider dateAdapter={AdapterDayjs}>
+//           <MobileDatePicker
+//             format="DD.MM.YYYY"
+//             onAccept={handleAcceptDate}
+//             value={dayjs(selectedDate)}
+//             maxDate={dayjs(currentDate)}
+//           />
+//           <DateRangeIcon />
+//         </LocalizationProvider>
+//       </DatePickerBoxCss>
+
+//       <DiaryListProductsBoxCss>
+//         {screenSize.width < theme.breakpoints.values.tablet ? (
+//           <DiarySearchProductMobile />
+//         ) : (
+//           <DiarySearchProduct />
+//         )}
+//       </DiaryListProductsBoxCss>
+
+//       <DiaryEatenProductsList day={dayInfo} />
+//     </>
+//   );
+// };
