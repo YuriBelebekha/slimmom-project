f500d0ad2c55f467646b8093d077d30b298fef8e
YuriBelebekha <105389957+YuriBelebekha@users.noreply.github.com>
2024-04-26 20:39:50 +0000

    Deploying to gh-pages from @ YuriBelebekha/slimmom-project@0c5ebdbdedb22ecbe9ffde0df4321d1ddd6c8db6 🚀

--- static/js/446.cebcb55c.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkslimmom_project=self.webpackChunkslimmom_project||[]).push([[446],{1446:function(n,c,e){e.r(c),e.d(c,{default:function(){return t}});var s=e(6907),i=e(8933),l=e(1080),r=e(184),t=function(){return(0,r.jsxs)(s.B6,{children:[(0,r.jsx)(s.ql,{children:(0,r.jsx)("title",{children:"SlimMom | Diary"})}),(0,r.jsx)(i.c,{func:function(n){console.log(n)}}),(0,r.jsx)(l.U,{})]})}}}]);
-//# sourceMappingURL=446.cebcb55c.chunk.js.map
